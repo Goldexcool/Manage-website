7e2757a1042e205b06cca10f09145b87d1b0aeab
Goldexcool <ogunseitangold105@gmail.com>
2023-09-13 14:15:14 +0100

    second commit

--- manage.js
+++ manage.js
@@ -9,5 +9,23 @@ tuggleBtn.onclick = function () {
   btn.classList.toggle("fa-x", isOpen);
 };
 
+var emailbtn = document.querySelector(".email_btn");
+var error = document.querySelector(".errormess");
+
+
+emailbtn.addEventListener("click", function (e) {
+    e.preventDefault()
+    var valid = document.getElementById("validemail").value
+
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(valid)) {
+    error.innerHTML = "Sign up Sucessful"
+    error.classList.add("error")
+    return true;
+  } else {
+    error.innerHTML = "Must enter a valid email"
+    error.classList.add("error")
+    return false;
+  }
+});
 
 
